575e4d2244bb0e5aea926e47196daf040e728ae1
Ankit Mishra <v-ankmishra@microsoft.com>
2024-03-01 22:00:35 +0530

    update

--- duplicateElementRemoval.js
+++ duplicateElementRemoval.js
@@ -9,3 +9,18 @@ for (let i = 0; i < arr.length; i++) {
 }
 
 console.log(arr)
+
+let arr_1 = [1, 2, 3, 4, 6, 7, 9, 1, 2, 4, 5];
+
+let result = arr_1.filter((value, index, self) => {
+    return self.indexOf(value) === index
+})
+
+console.log(result)
+
+let arr_2 = [1, 2, 3, 4, 6, 7, 9, 1, 2, 4, 5];
+
+let set = new Set(arr_2)
+let result_1 = Array.from(set)
+
+console.log(result_1)
